ae17483d224425da45a4e9cc6dedfc20c2d2cc27
aaravrailavender-source <aaravrailavender@gmail.com>
2025-11-21 12:53:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("BOT_TOKEN")  # Render → Environment → BOT_TOKEN
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is working!")
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text = update.message.text
-    await update.message.reply_text(f"You said: {text}")
+    await update.message.reply_text("Bot is running!")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
     app.run_polling()
 
 if __name__ == "__main__":
